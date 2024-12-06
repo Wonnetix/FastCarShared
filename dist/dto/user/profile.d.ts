942524fb1dd0e8dc91afd4499ae714282e27a7f0
MIP <mip.putalocura@gmail.com>
2024-12-06 10:56:54 +0100

    build: release version 1.3.1

--- dist/dto/user/profile.d.ts
+++ dist/dto/user/profile.d.ts
@@ -3,5 +3,13 @@ declare class UserProfileDto extends UserBaseRecord {
     id_user: string;
     bio?: string;
     profilePicture?: string;
+    isPublic?: boolean;
+    isActive?: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    _count: {
+        followers: number;
+        following: number;
+    };
 }
 export { UserProfileDto };
--- src/dto/user/profile.ts
+++ src/dto/user/profile.ts
@@ -4,6 +4,14 @@ class UserProfileDto extends UserBaseRecord {
   id_user: string;
   bio?: string;
   profilePicture?: string;
+  isPublic?: boolean;
+  isActive?: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  _count: {
+    followers: number;
+    following: number;
+  };
 }
 
 export { UserProfileDto };
